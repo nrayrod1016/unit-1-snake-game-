37cb8816807d8a933bdb94bd4eef9e24e9dc4d2d
Nick Rodriguez <nickrrodriguez@gmail.com>
2021-07-05 22:32:20 -0400

    added const 3D variables with pseudocode

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // possible threejs cloudflare link 
+ 
+
+
+//  Constant Variables 
+
+// will give this function 4 arguments (Field of View, Aspect Ratio, Near clipping place, Far clipping plane )
+const scene = new THREE.Scene()
+
+
+// 
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 ) 
+
+
+// 
+const renderer = new THREE.WebGLRenderer({ antalias: true})
